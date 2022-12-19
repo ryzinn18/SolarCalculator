7f91793f32f530ba12345ddeb807f0ef2879d487
Ryan Wright-Zinniger <ryr.zinn@gmail.com>
2022-12-19 14:11:13 -0800

    Opened up main() to run whole process

--- src/main.py
+++ src/main.py
@@ -33,17 +33,17 @@ def main():
         input_source=SAMPLES['csv_valid']
     )
     LOGGER.info(f'InputData successfully received for name {data_input.name}')
-    #
-    # # Get Solar Potential data
-    # data_solar_potential = sp.get_solar_potential(
-    #     address=data_input.address,
-    #     annual_consumption=data_input.consumption_annual
-    # )
-    # LOGGER.info(f'SolarPotentialData data successfully received for address {data_solar_potential.address}')
-    #
-    # # Get Results data
-    # data_results = res.get_results(input_data=data_input, solar_potential_data=data_solar_potential)
-    # LOGGER.info(f'ResultsData successfully received for {data_results.name}')
+
+    # Get Solar Potential data
+    data_solar_potential = sp.get_solar_potential(
+        address=data_input.address,
+        annual_consumption=data_input.consumption_annual
+    )
+    LOGGER.info(f'SolarPotentialData data successfully received for address {data_solar_potential.address}')
+
+    # Get Results data
+    data_results = res.get_results(input_data=data_input, solar_potential_data=data_solar_potential)
+    LOGGER.info(f'ResultsData successfully received for {data_results.name}')
 
     return data_input
 
