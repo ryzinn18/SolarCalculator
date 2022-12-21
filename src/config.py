8b443a121e35cc1d2abe6d20d12845936af050b2
Ryan Zinniger <78385496+ryzinn18@users.noreply.github.com>
2022-12-21 14:09:43 -0800

    Created config.py with placeholders

---
+++ src/config.py
@@ -0,0 +1,9 @@
+# ./src/config.py
+
+# REQUIRED: Paste in your NREL API Key here. Acquire your NREL API Key here:
+#   https://developer.nrel.gov/signup/
+nrel_api_key = ''
+
+# OPTIONAL: Paste in your Google Sheet ID only if you plan on using a Google sheet to input your data.
+# You need to run quickstart.py (found in ./src/archive/) in ./src/backend to set up your Goolge API credentials.
+google_api_sheet_id = ''
