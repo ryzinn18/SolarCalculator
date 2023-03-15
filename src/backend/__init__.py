f0ec21a71b7e34550a2c187c6a7667935c0772e2
Ryan Wright-Zinniger <ryr.zinn@gmail.com>
2023-03-15 12:07:55 -0700

    Updated to relative imports

--- src/backend/__init__.py
+++ src/backend/__init__.py
@@ -1,6 +1,6 @@
 # src/backend/__init_.py
-from utils import import_json, export_json, JSON, MONTHS_MAP, IntListMonthly, FloatListMonthly, \
+from ..utils import import_json, export_json, JSON, MONTHS_MAP, IntListMonthly, FloatListMonthly, \
     AmbiguousListMonthly, PandasDataFrame, InputData, Results, SolarPotentialData
-from backend.inputs import InputError, get_inputs, input_csv, input_xlsx, input_sheets
-from backend.solar_potential import get_solar_potential
-from backend.results import create_comparison_graph, get_data_df, get_results
+from .inputs import InputError, get_inputs, input_csv, input_xlsx, input_sheets
+from .solar_potential import get_solar_potential
+from .results import create_comparison_graph, get_data_df, get_results
