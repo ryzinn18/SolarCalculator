4dd18223e0ece3e3d0549cce23358fefb5bdd685
Ryan Wright-Zinniger <ryr.zinn@gmail.com>
2023-04-07 16:27:22 -0700

    Added function to show/hide loading page.

--- src/frontend/static/js/scripts.js
+++ src/frontend/static/js/scripts.js
@@ -1,12 +1,3 @@
-/*!
-* Start Bootstrap - Creative v7.0.6 (https://startbootstrap.com/theme/creative)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
-
 window.addEventListener('DOMContentLoaded', event => {
 
     // Navbar shrink function
@@ -57,3 +48,15 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+
+function showLoader() {
+    // Temporarily display loader page after submitting input
+
+    // Show the loader
+    document.getElementById('loading').style.display = "block";
+    // Remove loader after 12 seconds (request takes <= 12 seconds before timeout)
+    setTimeout(function() {
+        document.getElementById('loading').style.display = "none";
+    }, 12000);
+}
