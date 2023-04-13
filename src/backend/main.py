450c95d2849ebd561fa6781a78647c954068facb
Ryan Wright-Zinniger <ryr.zinn@gmail.com>
2023-04-13 10:25:01 -0700

    Updated []'s to .get()'s

--- src/backend/main.py
+++ src/backend/main.py
@@ -2,7 +2,7 @@
 # This is the main module for running this program.
 from logging import getLogger, basicConfig, ERROR
 
-from utils import check_http_response
+from utils import check_http_response, Status
 
 from inputs import input_handler
 from solar_potential import solar_potential_handler
@@ -29,32 +29,32 @@ def main_handler(event: dict, context) -> dict:
     data_input = input_handler(
         event=event,
     )
-    if not check_http_response(response_code=data_input["status"]["status_code"]):
+    if not check_http_response(response_code=data_input.get("status").get("status_code")):
         LOGGER.error(
-            f'InputData was returned with a {data_input["status"]["status_code"]} status code  '
-            f'and the following message: {data_input["status"]["message"]}')
-    LOGGER.info(f'InputData successfully received for uid: {data_input["uid"]}')
+            f'InputData was returned with a {data_input.get("status").get("status_code")} status code  '
+            f'and the following message: {data_input.get("status").get("message")}')
+    LOGGER.info(f'InputData successfully received for uid: {data_input.get("uid")}')
 
     # Get Solar Potential data
     data_solar_potential = solar_potential_handler(
         event=data_input,
     )
-    if not check_http_response(response_code=data_solar_potential["status"]["status_code"]):
+    if not check_http_response(response_code=data_solar_potential.get("status").get("status_code")):
         LOGGER.error(
-            f'InputData was returned with a {data_solar_potential["status"]["status_code"]} status code  '
-            f'and the following message: {data_solar_potential["status"]["message"]}')
-    LOGGER.info(f'SolarPotentialData data successfully received for uid: {data_solar_potential["uid"]}')
+            f'InputData was returned with a {data_solar_potential.get("status").get("status_code")} status code  '
+            f'and the following message: {data_solar_potential.get("status").get("message")}')
+    LOGGER.info(f'SolarPotentialData data successfully received for uid: {data_solar_potential.get("uid")}')
 
     # Get Results data
     data_results = results_handler(
         input_data=data_input,
         solar_data=data_solar_potential
     )
-    if not check_http_response(response_code=data_results["status"]["status_code"]):
+    if not check_object(data_results):
         LOGGER.error(
-            f'InputData was returned with a {data_results["status"]["status_code"]} status code  ' \
-            + f'and the following message: {data_results["status"]["message"]}')
-    LOGGER.info(f'Results data successfully received for: {data_results["uid"]}')
+            f'InputData was returned with a X status code  '
+            + f'and the following message: Y')
+    LOGGER.info(f'Results data successfully received for: {data_results.get("uid")}')
 
     return data_results
 
@@ -63,6 +63,6 @@ if __name__ == '__main__':
     from utils import import_json, SAMPLES
 
     OUT = main_handler(event=import_json(SAMPLES['event_valid_form']), context=None)
-
-    print("STATUS_CODE: ", OUT['status']['status_code'])
-    print("MOD_QUANTITY: ", OUT['mod_quantity'])
+    print(OUT)
+    # print("STATUS_CODE: ", OUT.get('status').get('status_code'))
+    # print("MOD_QUANTITY: ", OUT.get('mod_quantity'))
