a7bb13d3c5dc6cd8653b658389881c168663279c
Ryan Wright-Zinniger <ryr.zinn@gmail.com>
2023-04-25 20:04:04 -0700

    Updated for init infrastructure.

--- src/frontend/views.py
+++ src/frontend/views.py
@@ -1,16 +1,12 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import current_user
-import boto3
 
 from datetime import datetime as dt
-import json
 
-from .utils import MONTHS_MAP, DYNAMODB, post_item_to_dynamodb, clean_name, check_http_response
+from .initialize_inputs import *
+from .utils import MONTHS_MAP, clean_name
 
 views = Blueprint("views", __name__)
-
-DDB_NAME = "solarCalculatorTable-Inputs"
-LAMBDA = boto3.client('lambda', region_name='us-west-1')
 
 
 @views.route("/")
@@ -28,9 +24,9 @@ def documentation():
     return render_template("docs.html", user=current_user)
 
 
-@views.route("/run-tool", methods=["GET", "POST"])
-def run_tool():
-    """Run the Solar Calculator tool."""
+@views.route("/init-tool", methods=["GET", "POST"])
+def init_data():
+    """Initialize the Solar Calculator tool."""
 
     # Edge Case - return to create post page if method is not POST
     if request.method != "POST":
@@ -39,63 +35,54 @@ def run_tool():
     # Define request parameters from input form
     time_stamp = dt.now().__str__().replace(' ', '_').replace(':', '.')
     username = clean_name(request.form.get("name"))
-    uid = username + time_stamp
+    uid = f"{username}-{time_stamp}"
     address = request.form.get("address")
-    mod_kwh = request.form.get("mod_kwh")
+    mod_data = {
+        "mod_kwh": request.form.get("mod_kwh"),
+        "mod_price": request.form.get("mod_price"),
+    }
     monthly_data = {
         # Key=month (str): Value=[energyMonth, costMonth] (array[int, float])
     }
     for month in MONTHS_MAP.values():
         monthly_data[month] = [str(request.form.get(f"energy{month}")), str(request.form.get(f"cost{month}"))]
 
-    # Write initial input data to DynamoBD table: solarCalculator-Inputs
-    table_item = {
-        "uid": username + time_stamp,
-        "name": username,
+    # Validate Input Data
+    if not validate_init_data(username, address, mod_data, monthly_data):
+        return redirect(url_for("views.home", user=current_user))
+
+    # Call Lambda sc-be-solar for solar data
+    init_inputs = {
+        "uid": uid,
         "address": address,
-        "mod_kwh": mod_kwh,
-        "monthly_data": monthly_data
+        "capacity": 1
     }
-    db_response = post_item_to_dynamodb(DYNAMODB.Table(DDB_NAME), item=table_item)
-    if not check_http_response(response_code=db_response):
-        flash(
-            "Something happened while processing your request! Please try again.", category="error"
-        )
+    solar_init_data = get_solar_data(init_inputs)
+    if not solar_init_data:
         return redirect(url_for("views.home", user=current_user))
 
-    try:
-        for month in MONTHS_MAP.values():
-            monthly_data[month] = [float(request.form.get(f"energy{month}")), float(request.form.get(f"cost{month}"))]
-    except ValueError:
-        flash(
-            "You entered an incorrect or empty value! Ensure all your Consumption & Cost values are numbers and not "
-            "null.",
-            category="error"
-        )
-        return redirect(url_for("views.home", user=current_user))
+    # Calculate capacity and mod_quantity
+    capacity = suggest_capacity(monthly_data=monthly_data, annual_output=solar_init_data.get('output_annual'))
+    # noinspection PyTypeChecker
+    mod_quantity = suggest_mod_quantity(capacity=capacity, mod_rating=mod_data.get('mod_kwh'))
 
-    # Call main function to run the tool
-    input_item = {
-        "type": "form",
-        "form": {
-            "uid": uid,
-            "name": username,
-            "address": address,
-            "mod_kwh": mod_kwh,
-            "monthly_data": monthly_data
-        }
-    }
-    lambda_response = LAMBDA.invoke(
-        FunctionName='sc-be',
-        InvocationType='RequestResponse',
-        Payload=json.dumps(input_item)
+    # Store validated input data - async
+    store_inputs(
+        username=username,
+        time_stamp=time_stamp,
+        address=address,
+        monthly_data=monthly_data,
+        capacity=capacity,
+        mod_data=mod_data
     )
-    if not check_http_response(response_code=lambda_response["StatusCode"]):
-        flash(
-            "Something happened while processing your request! Please try again.", category="error"
-        )
-        return redirect(url_for("views.home", user=current_user))
 
-    results = json.loads(lambda_response['Payload'].read().decode("utf-8"))
+    init_data = {
+        "uid": uid,
+        "suggested_capacity": capacity,
+        "suggested_mod_quantity": mod_quantity,
+    }
 
-    return render_template("output.html", user=current_user, output=results, months=MONTHS_MAP)
+    """Write code to pass init_data to home page, expand the init_data section, and call get_results."""
+
+
+    # return render_template("output.html", user=current_user, output=results, months=MONTHS_MAP)
