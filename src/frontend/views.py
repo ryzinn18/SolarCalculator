c617d0b275f0617992953d1c8822b395c5d879a9
Ryan Wright-Zinniger <ryr.zinn@gmail.com>
2023-04-28 10:45:01 -0700

    Updated /run-tool view to /init-data

--- src/frontend/views.py
+++ src/frontend/views.py
@@ -1,10 +1,12 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask import Blueprint, render_template, request, redirect, url_for, flash, jsonify
 from flask_login import current_user
 
+import threading
 from datetime import datetime as dt
+from decimal import Decimal
 
 from .initialize_inputs import *
-from .utils import MONTHS_MAP, clean_name
+from .utils import MONTHS_MAP, clean_name, import_json
 
 views = Blueprint("views", __name__)
 
@@ -24,65 +26,74 @@ def documentation():
     return render_template("docs.html", user=current_user)
 
 
-@views.route("/init-tool", methods=["GET", "POST"])
-def init_data():
+@views.route("/init-data/")
+def init_tool():
     """Initialize the Solar Calculator tool."""
 
-    # Edge Case - return to create post page if method is not POST
-    if request.method != "POST":
-        return render_template("home.html", user=current_user)
+    print((request.args.get('energy').split(",")))
 
-    # Define request parameters from input form
+    # Define request parameters from request
     time_stamp = dt.now().__str__().replace(' ', '_').replace(':', '.')
-    username = clean_name(request.form.get("name"))
+    username = clean_name(request.args.get("name"))
     uid = f"{username}-{time_stamp}"
-    address = request.form.get("address")
-    mod_data = {
-        "mod_kwh": request.form.get("mod_kwh"),
-        "mod_price": request.form.get("mod_price"),
-    }
-    monthly_data = {
-        # Key=month (str): Value=[energyMonth, costMonth] (array[int, float])
-    }
-    for month in MONTHS_MAP.values():
-        monthly_data[month] = [str(request.form.get(f"energy{month}")), str(request.form.get(f"cost{month}"))]
-
+    address = request.args.get("address")
+    rating = request.args.get("rating")
+    energy_data = request.args.get("energy").split(",")
+    cost_data = request.args.get("cost").split(",")
     # Validate Input Data
-    if not validate_init_data(username, address, mod_data, monthly_data):
+    if not validate_init_data(username, address, rating, energy_data, cost_data):
+        # RETURN ERRANT JSON
         return redirect(url_for("views.home", user=current_user))
+    rating = float(rating)
+    energy_data = [int(n) for n in energy_data]
+    cost_data = [float(n) for n in cost_data]
 
-    # Call Lambda sc-be-solar for solar data
-    init_inputs = {
+    # Call Lambda sc-be-solar for init solar data
+    init_solar_inputs = {
         "uid": uid,
         "address": address,
         "capacity": 1
     }
-    solar_init_data = get_solar_data(init_inputs)
-    if not solar_init_data:
-        return redirect(url_for("views.home", user=current_user))
+    init_solar_data = get_solar_data(init_solar_inputs)
+    # if not init_solar_data:
+    #     # RETURN ERRANT JSON
+    #     return redirect(url_for("views.home", user=current_user))
 
     # Calculate capacity and mod_quantity
-    capacity = suggest_capacity(monthly_data=monthly_data, annual_output=solar_init_data.get('output_annual'))
+    capacity = suggest_capacity(energy_data, annual_output=init_solar_data.get('output_annual'))
     # noinspection PyTypeChecker
-    mod_quantity = suggest_mod_quantity(capacity=capacity, mod_rating=mod_data.get('mod_kwh'))
-
-    # Store validated input data - async
-    store_inputs(
-        username=username,
-        time_stamp=time_stamp,
-        address=address,
-        monthly_data=monthly_data,
-        capacity=capacity,
-        mod_data=mod_data
-    )
+    mod_quantity = suggest_mod_quantity(capacity=capacity, mod_rating=rating)
+    state = init_solar_data.get('state')
+    state_price = round(import_json('/Users/ryanwright-zinniger/Desktop/SolarCalculator/src/frontend/static/data/SolarCostData.json').get(state), 2)
+    if not state_price:
+        # Log Error
+        pass
+    total_price = round(state_price * capacity * 1000)
+    tax_credit = round(total_price * 0.3)
+    discount_price = round(total_price * 0.7)
 
     init_data = {
         "uid": uid,
-        "suggested_capacity": capacity,
-        "suggested_mod_quantity": mod_quantity,
+        "name": username,
+        "stage": "init",
+        "address": address,
+        "energy": energy_data,
+        "cost": [Decimal(str(n)) for n in cost_data],
+        "state": state,
+        "state_price": Decimal(str(state_price)),
+        "capacity": Decimal(str(capacity)),             # suggested
+        "mod_quantity": mod_quantity,                   # suggested
+        "tax_credit": tax_credit,
+        "total_price": total_price,
+        "discount_price": discount_price,
+        "status": {
+            "status_code": 200,
+            "message": "initialization steps completed."
+        },
     }
 
-    """Write code to pass init_data to home page, expand the init_data section, and call get_results."""
+    # Create and run thread to store init inputs
+    thread = threading.Thread(target=store_inputs, args=[init_data])
+    thread.start()
 
-
-    # return render_template("output.html", user=current_user, output=results, months=MONTHS_MAP)
+    return jsonify(init_data)
